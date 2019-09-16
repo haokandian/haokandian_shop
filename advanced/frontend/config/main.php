935b4d5579b17891fd3442c1c3c50ee976f02cb0
shandian909520 <51946564+shandian909520@users.noreply.github.com>
2019-09-16 10:35:01 +0800

    Update main.php
    修改后缀为json ，增加api 基类

--- advanced/frontend/config/main.php
+++ advanced/frontend/config/main.php
@@ -12,6 +12,21 @@ return [
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
+        
+        
+        'response' => [
+            'class' => 'yii\web\Response',
+            'on beforeSend' => function ($event) {
+                $response = $event->sender;
+                $response->data = [
+                    'success' => $response->isSuccessful,
+                    'code' => $response->getStatusCode(),
+                    'message' => $response->statusText,
+                    'data' => $response->data,
+                ];
+                $response->statusCode = 200;
+            },
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
@@ -32,7 +47,7 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'suffix'=>'.html',
+            'suffix'=>'.json',
             'rules' => [
             ],
         ],
