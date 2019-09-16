149fd52858b304a31d376842fb666b53950ee8b7
shandian909520 <51946564+shandian909520@users.noreply.github.com>
2019-09-16 14:08:37 +0800

    Update NpiController.php
    修改接口的表现形式

--- advanced/frontend/controllers/NpiController.php
+++ advanced/frontend/controllers/NpiController.php
@@ -14,19 +14,20 @@ use yii\web\Controller;
 
 class NpiController extends ApiController
 {
+    public $modelClass='frontend\models\NsCategory';   
     public function actionList(){
         $list = NsCategory::find()->where(['pid'=>0])->asArray()->all();
 
         $pid=Yii::$app->request->get('id',$list[0]['id']);
 
         $list2 = NsCategory::find()->where(['pid'=>$pid])->asArray()->all();
-       //    var_dump($list2);die;
         foreach($list2 as $k=>$v){
             $list2[$k]["goods_list"]=Nsgoods::find()->where(['category'=>$v['id']])->asArray()->all();
         }
         $data['stair']=$list;
         $data['second']=$list2;
        // print_r($data);die;
+        return $data;
         $data =array(
             "code"=>0,
             "message"=>"",
